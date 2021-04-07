4b59600b028b2662ccdc8bfd4de7b17a2dcfe1b0
colmo0912 <colmo0912@gmail.com>
2021-04-07 14:50:57 +0200

    aggiungo script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+var nome = "Caio! Qual'è il tuo nome?";
+
+prompt(nome);
+
+var cognome = "Qual'è il tuo cognome?"
+
+prompt(cognome)
+
+var colore = "Qual'è il tuo colore preferito?"
+
+prompt(colore)
+
+
+
